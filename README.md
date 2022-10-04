9046f45c4694e18079d0191ad1c0b0d7c606a8ca
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2022-10-05 01:27:33 +0200

    add audio volume functions, update readme

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ## A simple fanmade reskin and rework of Fallout FM
 I made it because I don't like the base css of their app but love the concept.
 ## It's a work in progress
-Be patient.
+Be patient it's a wip.
+It's hosted on Netlify and I use a _redirects conf file with it for bypassing Https call (the original station uses simple Http).
 #### Thanks to [Radio Fallout.FM](http://www.fallout.fm/)
 The project is based on theirs.
 I just streamed on their Icecast2 station, simplified code for desktop and pwa, and changed the whole design.
--- js/script.js
+++ js/script.js
@@ -1,9 +1,72 @@
 async function playAudio(libelle, audio_flux) {
     try {
         $("audio").remove();
-        $("body").append('<audio autoplay src="' + audio_flux + '" />'),
-            console.log('Playing: ' + libelle);
+        $("body").append('<audio id="audio" autoplay src="' + audio_flux + '" />');
+        $("#audio").prop("volume", $("#audioLevel").val());
+        console.log('Playing: ' + libelle);
     } catch (ex) {
         console.log('Failed to play: ' + libelle + ', Exception: ' + ex);
     }
 }
+
+async function upAudio() {
+    try {
+        var $input = $('#audioLevel');
+        $input.val(+$input.val() + 0.1);
+        if ($input.val() > 1)
+            $input.val(1);
+        $("#audio").prop("volume", $input.val());
+        setCookie("audioLevel", $input.val(), 365);
+    } catch (ex) {
+        console.log('Failed to turn up audio level, Exception: ' + ex);
+    }
+
+}
+
+async function downAudio() {
+    try {
+        var $input = $('#audioLevel');
+        $input.val(+$input.val() - 0.1);
+        if ($input.val() < 0)
+            $input.val(0);
+        $("#audio").prop("volume", $input.val());
+        setCookie("audioLevel", $input.val(), 365);
+    } catch (ex) {
+        console.log('Failed to turn down audio level, Exception: ' + ex);
+    }
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let audioLevel = getCookie("audioLevel");
+    if (audioLevel != "") {
+        $('#audioLevel').val(audioLevel);
+    } else {
+        setCookie("audioLevel", 0.5, 365);
+    }
+}
+
+checkCookie();
