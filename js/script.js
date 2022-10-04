06de2772dd03a6d179f346f6449df22ce4ac13e2
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2022-10-05 00:15:39 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 async function playAudio(libelle, audio_flux) {
     try {
         $("audio").remove();
-        $("body").append('<audio autoplay src="http://fallout.fm:8000/' + audio_flux + '" />'),
+        $("body").append('<audio autoplay src="' + audio_flux + '" />'),
             console.log('Playing: ' + libelle);
     } catch (ex) {
         console.log('Failed to play: ' + libelle + ', Exception: ' + ex);
