89d74fdd0243ca552495c2487af3e96006e5c049
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2022-10-08 00:48:43 +0200

    add a real ui, add a lot of javascript functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,61 @@
+const stationList = [
+    { id: 1, name: "Main Station", route: "/api/falloutfm1.ogg" },
+    { id: 2, name: "Fallout 76 Classical", route: "/api/falloutfm9.ogg" },
+    { id: 3, name: "Fallout 76 General", route: "/api/falloutfm10.ogg" },
+    { id: 4, name: "Fallout 4 Classical Radio", route: "/api/falloutfm7.ogg" },
+    { id: 5, name: "Fallout 4 Diamond City Radio", route: "/api/falloutfm6.ogg" },
+    { id: 6, name: "Fallout 4 MWTCF", route: "/api/falloutfm8.ogg" },
+    { id: 7, name: "Fallout 3 Galaxy News Radio", route: "/api/falloutfm2.ogg" },
+    { id: 8, name: "Fallout NV Radio New Vegas", route: "/api/falloutfm3.ogg" },
+    { id: 9, name: "Fallout 2 OST", route: "/api/falloutfm4.ogg" },
+    { id: 10, name: "Fallout 1 OST", route: "/api/falloutfm5.ogg" }
+];
+
+const falloutFmUri = "http://fallout.fm:8000";
+var actualStationId = null;
+var index = -1;
+
 async function clicChangeStation() {
-    console.log("You clicked clicChangeStation");
+    var uri = "";
+
+    if (actualStationId == null || actualStationId < 0) {
+        index = 0;
+    } else if (stationList.length == index + 1) {
+        index = 0;
+    }
+    else {
+        index = index + 1;
+    }
+
+    actualStationId = stationList[index].id;
+
+    if (window.location.href.indexOf("netlify") > -1) {
+        uri = stationList[index].route;
+    }
+    else {
+        uri = falloutFmUri + stationList[index].route.replace('/api', '');
+    }
+
+    try {
+        $("audio").remove();
+        $("body").append('<audio id="audio" autoplay src="' + uri + '" />');
+        $("#audio").prop("volume", $("#audioLevel").val());
+    } catch (ex) {
+        console.log('Failed to play: ' + stationList[index].name + ', Exception: ' + ex);
+    }
 }
 
 async function clicChangeVolume() {
-    console.log("You clicked clicChangeVolume");
+    try {
+        var $input = $('#audioLevel');
+        $input.val(+$input.val() + 0.1);
+        if ($input.val() > 1)
+            $input.val(0);
+        $("#audio").prop("volume", $input.val());
+        setCookie("audioLevel", $input.val(), 365);
+    } catch (ex) {
+        console.log('Failed to turn up audio level, Exception: ' + ex);
+    }
 }
 
 async function playAudio(libelle, audio_flux) {
@@ -28,7 +80,6 @@ async function upAudio() {
     } catch (ex) {
         console.log('Failed to turn up audio level, Exception: ' + ex);
     }
-
 }
 
 async function downAudio() {
@@ -41,6 +92,14 @@ async function downAudio() {
         setCookie("audioLevel", $input.val(), 365);
     } catch (ex) {
         console.log('Failed to turn down audio level, Exception: ' + ex);
+    }
+}
+
+async function stopAudio() {
+    try {
+        $("audio").remove();
+    } catch (ex) {
+        console.log('Failed to turn stop audio, Exception: ' + ex);
     }
 }
 
@@ -92,7 +151,21 @@ async function sleep(ms) {
 }
 
 checkCookie();
-// while (true) {
-//     checkAudioPaused();
-//     sleep(2000);
-// }
+
+$("#year").text(new Date().getFullYear());
+
+$(document).ready(function (e) {
+    $('img[usemap]').rwdImageMaps();
+});
+
+var tooltipSpanVolumeMouse = document.getElementById('tooltip-span-volume-mouse');
+var tooltipSpanStationMouse = document.getElementById('tooltip-span-station-mouse');
+
+window.onmousemove = function (e) {
+    var x = (e.clientX - 40) + 'px',
+        y = (e.clientY - 40) + 'px';
+    tooltipSpanVolumeMouse.style.top = y;
+    tooltipSpanVolumeMouse.style.left = x;
+    tooltipSpanStationMouse.style.top = y;
+    tooltipSpanStationMouse.style.left = x;
+};
