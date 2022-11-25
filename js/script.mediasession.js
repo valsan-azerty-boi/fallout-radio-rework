5e17e47166b9217271ae1a1eebdaa8c72387f7c1
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2022-11-25 12:46:39 +0100

    update mediasession

--- js/script.mediasession.js
+++ js/script.mediasession.js
@@ -8,8 +8,13 @@ if (('mediaSession' in navigator)) {
     $("audio")[0].session = navigator.mediaSession;
 
     const actionHandlers = [
-        ['play', async () => { console.log("Keypress on media button: play"); }],
-        ['pause', async () => { console.log("Keypress on media button: pause"); }],
+        ['play', async () => {
+            console.log("Keypress on media button: play");
+        }],
+        ['pause', async () => {
+            console.log("Keypress on media button: pause");
+
+        }],
         ['stop', async () => { console.log("Keypress on media button: stop"); }]
     ]
 
@@ -29,10 +34,15 @@ function ensurePlayingStation() {
         actualAudio[0] != null && isPlaying
     ) {
         actualAudio[0].play();
+
+        if (('mediaSession' in navigator)) {
+            navigator.mediaSession.metadata.title = actualStationName;
+            navigator.mediaSession.metadata.artist = "fallout-radio-rework";
+        }
     }
     else {
         actualAudio[0].pause();
     }
 }
 ensurePlayingStation();
-setInterval(ensurePlayingStation, 20);
+setInterval(ensurePlayingStation, 100);
